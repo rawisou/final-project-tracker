5f9156525742540d71e912f7c17050e14437d572
Rawisara Saman <rawiisara@gmail.com>
2022-06-09 23:37:00 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>FINAL PROJECT: RAWI EMMA SARA</h1>
+    <main className='project-card'>
+      <article className="card">
+        <h3><span className='today'>10 June</span></h3>
+        <p>- AddNote Component (both FE/BE)</p>
+        <p>- Edit and delete features</p>
+      </article>
+
+      <article className="card">
+        <h3><span>11 June</span></h3>
+        <p className='name'>Rawi off (wedding)</p>
+        <p>- Design planning</p>
+      </article>
+
+      <article className="card">
+        <h3><span>12 June</span></h3>
+      </article>
+
+      <article className="card">
+        <h3><span>13 June</span></h3>
+        <p className='name'>Emma off (working)</p>
+        <p>- Start working on PWA</p>
+        <p>- Demo page</p>
+      </article>
+
+      <article className="card">
+        <h3><span>14 June</span></h3>
+        <p className='name'>Emma off (working)</p>
+        <p>- Summary component</p>
+        <p>- Design conclusion and start the implementation</p>
+      </article>
+
+      <article className="card">
+        <h3><span>15 June</span></h3>
+        <p>- Summary component</p>
+        <p>- Design implementation</p>
+      </article>
+
+      <article className="card">
+        <h3><span>16 June</span></h3>
+        <p>- Design implementation</p>
+      </article>
+
+      <article className="card">
+        <h3><span>17 June</span></h3>
+        <p>- Design implementation</p>
+      </article>
+
+      <article className="card">
+        <h3><span>18 June</span></h3>
+        <p>- Deploy!!!!</p>
+        <p>- PWA should be in place. The app should be downloadable</p>
+      </article>
+
+      <article className="card">
+        <h3><span>19 June</span></h3>
+        <p>- TEST (Bug hunting)</p>
+      </article>
+
+      <article className="card">
+        <h3><span>20 June</span></h3>
+        <p>- FEATURE FREEZE! No more new features!</p>
+        <p>- TEST (Bug hunting)</p>
+        <p>- Big cleanup</p>
+      </article>
+
+      <article className="card">
+        <h3><span>21 June</span></h3>
+        <p>- FEATURE FREEZE! No more new features!</p>
+        <p>- Aim for handing in?</p>
+      </article>
+
+      <article className="card">
+        <h3><span>22 June</span></h3>
+        <p className='name'> Rawi off (Åland)</p>
+      </article>
+
+      <article className="card">
+        <h3><span>23 June</span></h3>
+        <h4> GRADUATION DAY</h4>
+      </article>
+
+
+    </main>
+    </>
   );
 }
 
